05dcaec08c60989a067b8d70d1535d4c5e10c825
shitai995 <16622140921@163.com>
2025-11-13 00:11:00 +0800

    添加脚本头部描述

---
+++ Assets/Script/test.cs
@@ -0,0 +1,21 @@
+// ========================================================
+// 作者：娇娇 
+// 版本：2022.3.62
+// 描述：
+// ========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
